526b32ef56d9bd7210630ed2f05263b0b452af0e
pritamkhande <pritamkhande216@gmail.com>
2025-05-25 20:19:11 +0530

    Actually ensure tailwind.config.js content path is correct

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html", // THIS LINE MUST BE PRESENT AND CORRECT
+    "./index.html",
   ],
+  // TEMPORARY LINE TO FORCE GIT CHANGE
   theme: {
     extend: {},
   },
