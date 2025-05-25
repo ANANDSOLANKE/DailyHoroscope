b5cbf976cacb01ca57adaf16ab359767cf254e11
pritamkhande <pritamkhande216@gmail.com>
2025-05-25 20:01:28 +0530

    Ensure tailwind.config.js content path is correct on GitHub

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html", // Path to your main HTML file
-    // Add any other HTML or JS files where you use Tailwind classes
+    "./index.html", // THIS LINE MUST BE PRESENT AND CORRECT
   ],
   theme: {
     extend: {},
