81003bd0a1f82c87f551307b37819dc9cc0fbe75
pritamkhande <pritamkhande216@gmail.com>
2025-05-25 18:28:35 +0530

    Initial project commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Path to your main HTML file
+    // Add any other HTML or JS files where you use Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
