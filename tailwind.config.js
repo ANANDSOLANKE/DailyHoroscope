4b6b29a4d99cf2b2ea9e57e974a0b2e26d9a2ab0
pritamkhande <pritamkhande216@gmail.com>
2025-05-25 20:34:53 +0530

    Confirmed and fixed tailwind.config.js content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./index.html", // THIS LINE MUST BE PRESENT AND CORRECT
   ],
+  // TEMPORARY LINE TO FORCE GIT CHANGE (or whatever you added)
   theme: {
     extend: {},
   },
